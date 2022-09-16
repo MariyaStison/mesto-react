9f728e8ad0cd37a4e3ecca458d9eec1e176d2683
Mariya Stison <ladykleo@yandex.ru>
2022-09-16 16:14:35 +0300

    fix: заполнить атрибут alt для тега img в компоненте ImagePopup

--- src/components/ImagePopup.js
+++ src/components/ImagePopup.js
@@ -6,7 +6,7 @@ function ImagePopup(props) {
     <div className='popup popup_type_view popup_opened'>
       <div className="popup__container-view">
         <button type="button" className="btn btn_type_close btn_type_close-view" aria-label="Закрыть" onClick={props.onClose}></button>
-        <img src={props.card.link} className="popup__img" alt="" />
+        <img src={props.card.link} className="popup__img" alt={props.card.name} />
         <h2 className="popup__img-title">{props.card.name}</h2>
       </div>
     </div>
