7f124daeb234682f6896e84000359a6f92d906e1
Mariya Stison <ladykleo@yandex.ru>
2022-09-16 16:13:11 +0300

    style: перенести свойства компонентов на новую строку

--- src/components/App.js
+++ src/components/App.js
@@ -40,23 +40,39 @@ function App() {
     
     <div className="page">
       <Header />
-        <Main onEditProfile = {handleEditProfileClick} onAddPlace={handleAddPlaceClick} onEditAvatar={handleEditAvatarClick} onCardClick={handleCardClick} />
+        <Main 
+          onEditProfile = {handleEditProfileClick}
+          onAddPlace={handleAddPlaceClick}
+          onEditAvatar={handleEditAvatarClick}
+          onCardClick={handleCardClick} />
         <Footer /> 
-        <PopupWithForm name="edit" title="Редактировать профиль" isOpen = {isEditProfilePopupOpen} onClose={closeAllPopups}>
-          <input type="text" id="name-input" placeholder="Имя" name="name" required className="input input_type_name" minLength="2" maxLength="40" />
-          <span className='input-error name-input-error'></span>
-          <input type="text" id="about-input" placeholder="О себе" name="about" className="input input_type_about" required minLength="2" maxLength="200" />
-          <span className='input-error about-input-error'></span>
+        <PopupWithForm
+          name="edit"
+          title="Редактировать профиль"
+          isOpen = {isEditProfilePopupOpen}
+          onClose={closeAllPopups}>
+            <input type="text" id="name-input" placeholder="Имя" name="name" required className="input input_type_name" minLength="2" maxLength="40" />
+            <span className='input-error name-input-error'></span>
+            <input type="text" id="about-input" placeholder="О себе" name="about" className="input input_type_about" required minLength="2" maxLength="200" />
+            <span className='input-error about-input-error'></span>
         </PopupWithForm>
-        <PopupWithForm name="add" title="Новое место" isOpen = {isAddPlacePopupOpen} onClose={closeAllPopups}>
-          <input type="text" id="title-input" placeholder="Название" name="name" required className="input input_type_name" minLength="2" maxLength="30" />
-          <span className='input-error title-input-error'></span>
-          <input type="url" id="link-input" placeholder="Ссылка на картинку" name="link" required className="input input_type_about"/>
-          <span className='input-error link-input-error'></span>
+        <PopupWithForm
+          name="add"
+          title="Новое место"
+          isOpen = {isAddPlacePopupOpen}
+          onClose={closeAllPopups}>
+            <input type="text" id="title-input" placeholder="Название" name="name" required className="input input_type_name" minLength="2" maxLength="30" />
+            <span className='input-error title-input-error'></span>
+            <input type="url" id="link-input" placeholder="Ссылка на картинку" name="link" required className="input input_type_about"/>
+            <span className='input-error link-input-error'></span>
         </PopupWithForm>
-        <PopupWithForm name="edit-avatar" title="Обновить аватар" isOpen = {isEditAvatarPopupOpen} onClose={closeAllPopups}>
-          <input type="url" id="avatar-link-input" placeholder="Ссылка на новый аватар" name="link" required className="input input_type_name" /> 
-          <span className='input-error avatar-link-input-error'></span> 
+        <PopupWithForm
+          name="edit-avatar"
+          title="Обновить аватар"
+          isOpen = {isEditAvatarPopupOpen}
+          onClose={closeAllPopups}>
+            <input type="url" id="avatar-link-input" placeholder="Ссылка на новый аватар" name="link" required className="input input_type_name" /> 
+            <span className='input-error avatar-link-input-error'></span> 
         </PopupWithForm>
         <PopupWithForm name="confirm" title="Вы уверенны?" />
         <ImagePopup card={selectedCard} onClose={closeAllPopups}/>
